3cb3eb93943ffd2df40110fe764757920482c00d
mematrix <mematrix@users.noreply.github.com>
2022-05-16 02:30:03 +0800

    Add specialization for BlockingQueue with inner blocking queue.

--- queue/blocking_queue.hpp
+++ queue/blocking_queue.hpp
@@ -15,7 +15,29 @@
 
 namespace sc {
 
-template<typename Queue>
+namespace impl {
+
+template<typename T>
+class HasDequeue
+{
+    using yes_type = char;
+    using no_type = int;
+    static_assert(sizeof(yes_type) != sizeof(no_type));
+
+    // Only need the declaration, add an implementation to suppress the IDE warning.
+    template<typename C>
+    [[maybe_unused]] static yes_type test(decltype(&C::dequeue)) { return {}; }
+
+    template<typename>
+    [[maybe_unused]] static no_type test(...) { return {}; }
+
+public:
+    static constexpr bool value = sizeof(test<T>(nullptr)) == sizeof(yes_type);
+};
+
+}
+
+template<typename Queue, bool = impl::HasDequeue<Queue>::value>
 class BlockingQueue
 {
     template<typename>
@@ -79,6 +101,57 @@ private:
 
     std::mutex mtx_;
     std::condition_variable cond_var_;
+};
+
+template<typename Queue>
+class BlockingQueue<Queue, true>
+{
+    template<typename>
+    inline static constexpr bool dependent_false_v = false;
+
+public:
+    using value_type = typename Queue::value_type;
+    using reference = typename Queue::reference;
+    using const_reference = typename Queue::reference;
+
+    template<typename... Args>
+    explicit BlockingQueue(Args &&... args) noexcept(std::is_nothrow_constructible_v<Queue, Args...>)
+            : queue_(std::forward<Args>(args)...)
+    {
+    }
+
+    BlockingQueue(const BlockingQueue &) noexcept(std::is_nothrow_copy_constructible_v<Queue>) = default;
+
+    BlockingQueue(BlockingQueue &&) noexcept(std::is_nothrow_move_assignable_v<Queue>) = default;
+
+    BlockingQueue &operator=(const BlockingQueue &) noexcept(std::is_nothrow_copy_assignable_v<Queue>) = default;
+
+    BlockingQueue &operator=(BlockingQueue &&) noexcept(std::is_nothrow_move_assignable_v<Queue>) = default;
+
+    template<typename V>
+    void enqueue(V &&v)
+    {
+        if constexpr(std::is_same_v<value_type, std::remove_cvref_t<V>>) {
+            queue_.enqueue(std::forward<V>(v));
+        } else if constexpr(std::is_constructible_v<V, value_type>) {
+            queue_.enqueue(value_type(std::forward<V>(v)));
+        } else {
+            static_assert(dependent_false_v<V>, "Type is not expected.");
+        }
+    }
+
+    auto try_dequeue()
+    {
+        return queue_.try_dequeue();
+    }
+
+    auto dequeue()
+    {
+        return queue_.dequeue();
+    }
+
+private:
+    Queue queue_;
 };
 
 }
--- test/blocking_queue_test.cpp
+++ test/blocking_queue_test.cpp
@@ -12,8 +12,23 @@
 constexpr uint32_t ProducerCount = 4;
 constexpr uint32_t ConsumerCount = 2;
 
+struct ObjHasDequeue
+{
+    char value = 0;
+
+    char &dequeue() { return value; }
+};
+
+struct ObjNoDequeue
+{
+};
+
 int main()
 {
+    std::cout << std::boolalpha;
+    std::cout << "ObjHasDequeue check: " << sc::impl::HasDequeue<ObjHasDequeue>::value << std::endl;
+    std::cout << "ObjNoDequeue check: " << sc::impl::HasDequeue<ObjNoDequeue>::value << std::endl;
+
     sc::BlockingQueue<sc::mpmc::ArrayListQueue<Task>> queue;
 
     queue.enqueue(Task{});
